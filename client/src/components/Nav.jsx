61c468b5487baa3f324aeaa134accdfe217f8663
codify01 <amoleuthman@gmail.com>
2024-07-24 21:50:29 +0100

    Sorted the issue of reloading

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -9,11 +9,11 @@ const Nav = () => {
     <div className='bg-white h-28 flex justify-between items-center container'>
     <NavLink to={'/'}><div className='w-40 mt-5'> <img src={Logo} alt="" /></div></NavLink>
     <ul className='hidden  text-whit md:flex justify-end gap-16 items-center h-full me-9 text-lg'>
-      <li> <NavLink to="/" className="text-white">Home</NavLink></li>
-      <li> <NavLink to="/about-us">About Us</NavLink></li>
-      <li> <NavLink to="/services">Services</NavLink></li>
-      <li> <NavLink to="/categories">Categories</NavLink></li>
-      <li> <NavLink to="/partner">Partner With Us</NavLink></li>
+      <NavLink to="/"> <li  className="text-white">Home</li></NavLink>
+      <NavLink to="/about-us"> <li >About Us</li></NavLink>
+      <NavLink to="/services"> <li >Services</li></NavLink>
+      <NavLink to="/categories"> <li >Categories</li></NavLink>
+      <NavLink to="/partner-with-us"> <li >Partner With Us</li></NavLink>
     </ul>
     </div>
     </>
